53341e9d5e9a8b239f30748522b26a854e0d680b
Yeilyn Durán Sierra <yeilynds@ufps.edu.co>
2024-06-18 17:54:58 -0500

    Intercambio inicial de proyecto

---
+++ Moneda.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Moneda here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Moneda
+{
+    // instance variables - replace the example below with your own
+    private int idmoneda;
+    private String nombre;
+    private String simbolo;
+    
+}
---
+++ producto.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class producto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class producto
+{
+   private int idproducto;
+   private String codigo;
+   private float precio;
+   private int stock;
+   
+   public producto(int idproducto, String codigo, float precio, int stock){}
+   
+}
